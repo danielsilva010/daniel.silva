73a047a751cd9022131d6fe244955fb358973b86
danielsilva010 <103467713+danielsilva010@users.noreply.github.com>
2023-03-19 08:54:39 -0400

    Update script.php

--- script.php
+++ script.php
@@ -1,16 +1,1 @@
-<?php
-  // Get the form data
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $message = $_POST['message'];
 
-  // Send the email
-  $to = 'nutriseamoss@gmail.com';
-  $subject = 'New Contact Form Submission';
-  $body = "Name: $name\nEmail: $email\nMessage:\n$message";
-  $headers = "From: $email\r\nReply-To: $email\r\n";
-  mail($to, $subject, $body, $headers);
-
-  // Redirect the user to a thank-you page
-  header('Location: thank-you.html');
-?>
